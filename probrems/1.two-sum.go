145fa77cc81aefa3d6531a833f5dc1ea6d945be3
pottyasu <pottyasu2001@gmail.com>
2022-02-12 00:25:18 +0900

    solve probrem No.1

---
+++ probrems/1.two-sum.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+
+	var result []int
+	hashmap := make(map[int]int)
+
+	for i, v1 := range nums {
+		comp := target - v1
+		if _, ok := hashmap[comp]; ok && hashmap[comp] != i {
+			result = append(result, hashmap[comp], i)
+			break
+		}
+		hashmap[v1] = i
+	}
+
+	return result
+}
+
+// @lc code=end
+
